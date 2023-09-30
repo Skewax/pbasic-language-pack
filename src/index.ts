6377b47a15688fdc4a6453bd76913ad02063ba58
Floofer++ <56936160+dylanrafael05@users.noreply.github.com>
2023-09-29 23:06:51 -0400

    added various control structures

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import {parser} from "./syntax.grammar"
+import {SyntaxNodeRef} from "@lezer/common"
 import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
 import {styleTags, tags as t} from "@lezer/highlight"
+import {readFileSync} from "fs";
 
 export const PBasic = LRLanguage.define({
   name: 'pbasic',
@@ -21,3 +23,33 @@ export const PBasic = LRLanguage.define({
 export function pbasic() {
   return new LanguageSupport(PBasic)
 }
+
+export function test_for(file: string) {
+  const filetext = readFileSync(file).toString();
+  const tree = parser.parse(filetext);
+
+  let indent = 0;
+  let out = "";
+
+  tree.iterate({
+    enter: function(node: SyntaxNodeRef)
+    {
+      out += "\n" + "  ".repeat(indent) + node.name + "(";
+      indent++;
+    },
+
+    leave: function(node: SyntaxNodeRef)
+    {
+      out += ")";
+
+      if(node.node.nextSibling) out += ", ";
+      else out += "\n" + "  ".repeat(indent - 2)
+
+      indent--;
+    }
+  });
+
+  console.log(out);
+}
+
+test_for("example-code/ex1.txt")
